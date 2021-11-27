3b9a878666341f8f8ee0f10b75e29325ea3a3b98
Gaston Pedraza <sleepwalkgaston08@gmail.com>
2021-11-27 12:39:56 -0300

    set decimal places

--- migrations/20211127014219-create-product.js
+++ migrations/20211127014219-create-product.js
@@ -18,7 +18,7 @@ module.exports = {
         type: Sequelize.STRING
       },
       price: {
-        type: Sequelize.DECIMAL
+        type: Sequelize.DECIMAL(10, 2)
       },
       createdAt: {
         allowNull: false,
